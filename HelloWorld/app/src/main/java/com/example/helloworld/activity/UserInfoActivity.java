eb52d9b1e5e716991682b11659f949dafed4256d
yexiaoci <2871238016@qq.com>
2019-09-17 10:12:55 +0800

    个人信息修改界面

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/activity/UserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.helloworld.R;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
