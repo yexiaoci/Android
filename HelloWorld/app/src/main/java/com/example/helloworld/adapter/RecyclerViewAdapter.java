553a5fcc22b6bd53d4b2afd02d3507730cb0549b
yexiaoci <2871238016@qq.com>
2019-09-27 09:07:30 +0800

    动态设置TextView背景的shape填充色

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,66 @@
+package com.example.helloworld.adapter;
+
+
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.helloworld.R;
+import com.example.helloworld.entity.Exercise;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private List<Exercise> exercises;
+
+    public RecyclerViewAdapter(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_exercise, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        // 设置Item点击的监听器
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 跳转到习题详情页
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Exercise exercise = exercises.get(position);
+        holder.tvOrder.setText(String.valueOf(exercise.getId()));
+        holder.tvTitle.setText(exercise.getTitle());
+        holder.tvSubTitle.setText(exercise.getSubTitle());
+
+        // 设置圆角背景的颜色
+        GradientDrawable drawable = (GradientDrawable) holder.tvOrder.getBackground();
+        drawable.setColor(Color.parseColor(exercise.getBgColor()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return exercises.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvOrder, tvTitle, tvSubTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvOrder = itemView.findViewById(R.id.tv_order);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            tvSubTitle = itemView.findViewById(R.id.tv_sub_title);
+        }
+    }
+}
---
+++ HelloWorld/app/src/main/java/com/example/helloworld/fragment/RecyclerViewFragment.java
@@ -0,0 +1,162 @@
+package com.example.helloworld.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.niit.android.course.R;
+import edu.niit.android.course.adapter.RecyclerViewAdapter;
+import edu.niit.android.course.entity.Exercise;
+import edu.niit.android.course.entity.Exercise1;
+import edu.niit.android.course.utils.IOUtils;
+
+public class RecyclerViewFragment extends Fragment {
+    private List<Exercise> exercises;
+
+    public RecyclerViewFragment() {
+        // Required empty public constructor
+    }
+
+    public static RecyclerViewFragment newInstance(String param) {
+        RecyclerViewFragment fragment = new RecyclerViewFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("param", param);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // 1. 初始化数据
+        initData();
+        // 2. 获取控件
+        View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        // 3. 设置布局和分割线
+        LinearLayoutManager manager = new LinearLayoutManager(container.getContext());
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(container.getContext(),
+                DividerItemDecoration.VERTICAL));
+        // 4. 创建适配器
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(exercises);
+        // 5. 设置适配器
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    private void initData() {
+        exercises = new ArrayList<>();
+
+        try {
+            InputStream input = getResources().getAssets().open("exercise_title.json");
+            String json = IOUtils.convert(input, StandardCharsets.UTF_8);
+            exercises = IOUtils.convert(json, Exercise.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initExercises() {
+        exercises = new ArrayList<>();
+        for(int i = 0; i < 15; i++) {
+            Exercise exercise = new Exercise();
+            exercise.setId(i + 1);
+            switch (i) {
+                case 0:
+                    exercise.setTitle("第1章 Android基础入门");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_1);
+                    break;
+                case 1:
+                    exercise.setTitle("第2章 探究活动Activity");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_2);
+                    break;
+                case 2:
+                    exercise.setTitle("第3章 Android UI开发");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_3);
+                    break;
+                case 3:
+                    exercise.setTitle("第4章 探究碎片Fragment");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_4);
+                    break;
+                case 4:
+                    exercise.setTitle("第5章 广播接收者");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_5);
+                    break;
+                case 5:
+                    exercise.setTitle("第6章 数据存储");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_1);
+                    break;
+                case 6:
+                    exercise.setTitle("第7章 内容提供者");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_2);
+                    break;
+                case 7:
+                    exercise.setTitle("第8章 手机多媒体");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_3);
+                    break;
+                case 8:
+                    exercise.setTitle("第9章 网络编程");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_4);
+                    break;
+                case 9:
+                    exercise.setTitle("第10章 服务");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_5);
+                    break;
+                case 10:
+                    exercise.setTitle("第11章 基于位置的服务");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_1);
+                    break;
+                case 11:
+                    exercise.setTitle("第12章 Material Design实战");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_2);
+                    break;
+                case 12:
+                    exercise.setTitle("第13章 高级技巧");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_3);
+                    break;
+                case 13:
+                    exercise.setTitle("第14章 开发天气App");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_4);
+                    break;
+                case 14:
+                    exercise.setTitle("第15章 项目发布上线");
+                    exercise.setSubTitle("共计5题");
+                    exercise.setBackground(R.mipmap.ic_exercise_bg_5);
+                    break;
+                default:
+                    break;
+            }
+            exercises.add(exercise);
+        }
+    }
+}
